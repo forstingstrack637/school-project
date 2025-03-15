baffaa874b5bfa52651c70a4c0ae262575de21e0
forstingstrack637 <forstingstrack637@gmail.com>
2025-03-15 21:09:20 +0000

    Add 10.rs

---
+++ src/10.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 10;
+    if x < 5 {
+        println!("x is less than 5");
+    } else if x == 5 {
+        println!("x is equal to 5");
+    } else {
+        println!("x is greater than 5");
+    }
+}
